d28e5d740624c44f172022543eefa4b49285204d
Alexander <postoevalex@gmail.com>
2024-08-06 21:05:29 +0700

    Добавил веб сервер

--- Program.cs
+++ Program.cs
@@ -1,29 +1,104 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
 using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace TgBotDemo
 {
     internal class Program
     {
+        private static TelegramBotClient botClient;
+        private static string userChatId = "1842171326"; // Замените на ваш chat_id пользователя
+
         static void Main(string[] args)
         {
             Console.WriteLine("Bot starting");
-            var botClient = new TelegramBotClient("6408930022:AAFPLT5Onn9IXfTZONpu3G8ktldCovduapo");
+            botClient = new TelegramBotClient("6408930022:AAFPLT5Onn9IXfTZONpu3G8ktldCovduapo");
             botClient.StartReceiving(HandleUpdateBot, HandleErrorBot);
             Console.WriteLine("Bot started");
+
+            // Запуск веб-сервера
+            Task.Run(() => StartWebServer());
+
             Console.ReadLine();
         }
+
         private static async Task HandleUpdateBot(ITelegramBotClient botClient, Update update, CancellationToken token)
         {
             var msg = update.Message;
+
+            if (msg?.Chat.Type == ChatType.Group || msg?.Chat.Type == ChatType.Supergroup || msg?.Chat.Type == ChatType.Channel)
+            {
+                Console.WriteLine($"Сообщение из группового чата. Chat.Id = {msg.Chat.Id}");
+                return;
+            }
+
             if (msg?.Text != null)
             {
+                // Блок проверок команд
+                switch (msg.Text)
+                {
+                    case "/start_notify_teamcity":
+                        await botClient.SendTextMessageAsync(msg.Chat.Id, "Вы подписаны на уведомления TeamCity.");
+                        return;
+
+                    case "/end_notify_teamcity":
+                        await botClient.SendTextMessageAsync(msg.Chat.Id, "Вы отписаны от уведомлений TeamCity.");
+                        return;
+                    case "/t1":
+                        await botClient.SendTextMessageAsync(msg.Chat.Id, "Тест1");
+                        Console.WriteLine("Тест1");
+                        return;
+                    case "/t2":
+                        await botClient.SendTextMessageAsync(msg.Chat.Id, "Тест2");
+                        Console.WriteLine("Тест2");
+                        return;
+                }
+
+                if (msg.Text.Contains("teamcity", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Сообщение от TeamCity пришло");
+                }
+
+                // Попытка десериализовать текст сообщения в JSON
+                try
+                {
+                    var payload = JsonConvert.DeserializeObject<JObject>(msg.Text);
+
+                    if (payload != null && payload.ContainsKey("source"))
+                    {
+                        var source = payload["source"].ToString();
+                        if (source.ToLower() == "teamcity")
+                        {
+                            Console.WriteLine($"Перед обработкой \n {payload}");
+                            await HandleTeamCityMessage(botClient, userChatId, payload);
+                            return;
+                        }
+                    }
+                }
+                catch (JsonException ex)
+                {
+                    Console.WriteLine($"Ошибка десериализации JSON: {ex.Message}");
+                }
+
+                // Начало расчета гипотенузы
                 if (msg.Text.Contains("/start"))
                 {
-                    await botClient.SendTextMessageAsync(msg.Chat.Id, $"Бот умеет расчитывать гипотенузу прямоугольного треугольники по двум катетам. " +
-                        $"Можно вводить дробные значения.Для дробных значений необходимо использовать запятую. Для расчета введите два положительных числа через пробел");
+                    await botClient.SendTextMessageAsync(msg.Chat.Id, $"Бот умеет рассчитывать гипотенузу прямоугольного треугольника по двум катетам. " +
+                        $"Можно вводить дробные значения. Для дробных значений необходимо использовать запятую. Для расчета введите два положительных числа через пробел");
                     return;
                 }
 
@@ -42,15 +117,16 @@ namespace TgBotDemo
                 if (!secondLeg.IsValid) return;
 
                 double hypotenuse = Math.Sqrt(firstLeg.Value * firstLeg.Value + secondLeg.Value * secondLeg.Value);
-                Console.WriteLine($"Гипотинуза = {hypotenuse}");
+                Console.WriteLine($"Гипотенуза = {hypotenuse}");
 
                 await botClient.SendTextMessageAsync(msg.Chat.Id, $"Длина гипотенузы = {hypotenuse}");
             }
         }
+
         private static async Task<LegParseData> CheckNumber(string number, string serial, ITelegramBotClient botClient, ChatId id)
         {
             CultureInfo ruCulture = new CultureInfo("ru-RU");
-            if (!float.TryParse(number, ruCulture, out float leg))
+            if (!float.TryParse(number, NumberStyles.Float, ruCulture, out float leg))
             {
                 await botClient.SendTextMessageAsync(id, $"{serial} значение не число. Нужно ввести 2 положительных числа через пробел");
                 return new LegParseData { IsValid = false };
@@ -63,16 +139,62 @@ namespace TgBotDemo
             }
             return new LegParseData { IsValid = true, Value = leg };
         }
+
         private struct LegParseData
         {
             public bool IsValid;
             public float Value;
         }
+
+        private static async Task HandleTeamCityMessage(ITelegramBotClient botClient, ChatId chatId, JObject payload)
+        {
+            Console.WriteLine("Обработчик сообщений запущен");
+            var response = "Получены данные от TeamCity:\n";
+            response += $"Source: {payload["source"]}\n";
+
+            foreach (var property in payload.Properties().Where(p => p.Name != "source"))
+            {
+                response += $"{property.Name}: {property.Value}\n";
+            }
+
+            await botClient.SendTextMessageAsync(chatId, response);
+        }
+
         private static Task HandleErrorBot(ITelegramBotClient client, Exception exception, CancellationToken token)
         {
             Console.WriteLine($"Bot error \n {exception}");
             return Task.CompletedTask;
         }
+
+        private static void StartWebServer()
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://localhost:5000") // Замените на нужный вам порт
+                .ConfigureServices(services => services.AddSingleton(botClient))
+                .Configure(app => app.Run(async context =>
+                {
+                    if (context.Request.Method == "POST")
+                    {
+                        using (var reader = new StreamReader(context.Request.Body, Encoding.UTF8))
+                        {
+                            var requestBody = await reader.ReadToEndAsync();
+                            Console.WriteLine($"Получено сообщение: {requestBody}");
+                            
+                            // Отправка сообщения пользователю в Telegram
+                            await botClient.SendTextMessageAsync(userChatId, $"Новое сообщение:\n{requestBody}");
+                            context.Response.StatusCode = (int)HttpStatusCode.OK;
+                            await context.Response.WriteAsync("Message received and sent to Telegram.");
+                        }
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+                    }
+                }))
+                .Build();
+
+            host.Run();
+        }
     }
 }
-
